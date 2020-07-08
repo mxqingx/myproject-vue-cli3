4c47870aaf0726efe670bb3fb7b9f6c186d68d82
xiongqiang <qiang.xiong@dmall.com>
2020-07-08 17:55:29 +0800

    selectMore.vue

--- src/main.js
+++ src/main.js
@@ -4,19 +4,23 @@ import router from "./router";
 import store from "./store";
 import * as filters from "./filters"; // global filters
 import Mixin from "./mixins";
+import { Button, Select, Option } from 'element-ui';
 Vue.mixin(Mixin);
-// import echarts from "echarts";
-// Vue.prototype.$echarts = echarts;
-// 引入loadsh
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Option)
+    // import echarts from "echarts";
+    // Vue.prototype.$echarts = echarts;
+    // 引入loadsh
 import _ from "lodash";
 Vue.prototype._ = _;
 Vue.config.productionTip = false;
 // register global utility filters
 Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key]);
+    Vue.filter(key, filters[key]);
 });
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    版本:{{process}}
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      process: process
+    }
   }
 };
 </script>
