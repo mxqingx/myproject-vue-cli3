9530ea8955b178fcade641f32b4eb73f08069a92
xiongqiang <qiang.xiong@dmall.com>
2020-07-08 18:21:04 +0800

    自定义指令注册

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from "./store";
 import * as filters from "./filters"; // global filters
 import Mixin from "./mixins";
 import { Button, Select, Option } from 'element-ui';
+// 自定义指令
+import * as directives from './directives/index.js'
+// 注册
+Object.keys(directives).forEach(k => Vue.directive(k, directives[k]))
 Vue.mixin(Mixin);
 Vue.use(Button)
 Vue.use(Select)
